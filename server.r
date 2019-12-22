491813795e143faa55dce42f0a5b6f036de3e630
ParthThakur <parththakur@gmail.com>
2019-12-22 21:44:53 +0000

    Add documentation to code

--- server.r
+++ server.r
@@ -11,20 +11,25 @@ api_key <- readtext('secrets.txt')$text
 
 server <- function(input, output, session) {
     
+    # Fetch data from Quandl API.
     eod_data <- reactive(read.csv(paste(main_url, input$company_code, '/data.csv?api_key=', api_key,
                                sep='')))
     
+    # Display raw data.
     output$company_eod <- renderTable(eod_data())
-
+    
+    # Generate Candle-Stick chart.
     output$eod_plot <- renderPlot(eod_data() %>%
                                     ggplot(aes(x=Date, y=Close)) +
                                     geom_candlestick(aes(open=Open, high=High, low=Low, close=Close)) +
                                     theme_tq())
     
+    # Descriptive Statistics.
     output$summary_title <- renderText(paste('Descriptive statistics for', input$company_code))
     output$summary <- renderPrint({summary(eod_data()[, c('Open', 'Close', 'High', 'Low')])})
     output$summary_first <- renderPlot(ggplot(melt(eod_data()[ ,c('Open', 'Close', 'High', 'Low')]), 
                                               aes(variable, value)) +
-      geom_boxplot())
+                                       geom_boxplot())
+    
   
 }
--- ui.r
+++ ui.r
@@ -1,9 +1,13 @@
 library(shiny)
+
+# Grid layout dimensions
 sidebar = 3
 main_panel = 12 - sidebar
 
+# Main panel tab for Candle-Stick plot
 plot_tab <- tabPanel('Plot', h4(textOutput('plot_title')), plotOutput('eod_plot'))
 
+# Main Panel tab for descriptive statistics.
 summary_tab <- tabPanel('Summary',
                         h4(textOutput('summary_title')),
                         fluidRow(
@@ -14,6 +18,7 @@ summary_tab <- tabPanel('Summary',
                           column(main_panel, plotOutput('summary_first'))
                         ))
 
+# Main Panel tab for raw data.
 table_tab <- tabPanel('Table', tableOutput('company_eod'))
 
 
