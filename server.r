9787cf2054fc56960169f955c9b7df7072528d40
ParthThakur <parththakur@gmail.com>
2019-12-22 19:39:18 +0000

    Add candlestick chart

--- server.r
+++ server.r
@@ -1,4 +1,8 @@
 library(readtext)
+library(ggplot2)
+
+library(tidyverse)
+library(tidyquant)
 
 main_url <- "https://www.quandl.com/api/v3/datasets/WIKI/"
 api_key <- readtext('secrets.txt')$text
@@ -9,5 +13,13 @@ server <- function(input, output, session) {
                                sep='')))
     
     output$company_eod <- renderTable(eod_data())
+    
+    output$summary <- renderPrint({summary(eod_data()[, c('Open', 'Close', 'High', 'Low')]
+                                           )})
+
+    output$eod_plot <- renderPlot(eod_data() %>%
+                                    ggplot(aes(x=Date, y=Close)) +
+                                    geom_candlestick(aes(open=Open, high=High, low=Low, close=Close)) +
+                                    theme_tq())
   
 }
